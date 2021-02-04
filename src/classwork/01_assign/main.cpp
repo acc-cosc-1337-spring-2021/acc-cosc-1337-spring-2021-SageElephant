1a733a8b1b45147eb2cc6adb7e957b03ad3190f9
SageElephant <timothy.smithii@g.austincc.edu>
2021-02-04 04:05:49 +0000

    Homework 1:  00 Homework Classwork 01_assign program

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,22 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
+#include <iostream>
+using namespace std;
 
 
 
 int main() 
 {
-
+  int num;
+	cout << "Enter your number: ";
+	cin >> num;
+	int result;
+	result = multiply_numbers(num);
+	cout << "Your number multiplied by 5 = " << result << endl;
+	
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout << "The second result is: 4 x 5 = " << result << endl;
 	return 0;
 }
-
