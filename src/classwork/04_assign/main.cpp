3ff7af5f9ac5e9143b45b6e612340364f907fc4d
SageElephant <timothy.smithii@g.austincc.edu>
2021-03-02 04:07:30 +0000

    Homework 4 Submission

--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,8 @@
 //write includes statements
-
+#include <iostream>
+#include "loops.h"
 //write using statements for cin and cout
-
+using std::cin; using std::cout;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +11,26 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+  int num;
+  char go_again;
+  
+  do
+  {
+    do
+    {
+      cout << "Enter an integer number between 1 and 10: \n";
+      cin >> num;
+    }
+    while (!(num >= 1 && num <= 10));
+    
+    cout << "The factorial of " << num << " is " << factorial(num) << "\n";
 
+    cout << "Would you like to enter another number? Type 'Y' to continue.";
+    cin >> go_again;
+
+  }
+  while (go_again == 'Y');
+  
+  cout << "Thanks! The program has finished. \n";
 	return 0;
 }
