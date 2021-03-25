b5f59c64fee87eacdb6d087bc59e1106bc1eb28f
SageElephant <timothy.smithii@g.austincc.edu>
2021-03-25 14:07:14 +0000

    HW 5 Submission TSmith

--- src/homework/04_iteration/dna.h
+++ src/homework/04_iteration/dna.h
@@ -4,16 +4,18 @@
 Write prototype for function get_gc_content that accepts
 a const reference string parameter and returns a double
 */
-
+double get_gc_content(const std::string& dna);
 
 /*
 Write prototype for function get_reverse_string that 
 accepts a string parameter and returns a string
 */
 
+std::string get_reverse_string(std::string dna);  /* Note: One part of the HW description said to use the function name reverse_string and a different part said get_reverse_string. I used the latter. */
 
 /*
 Write prototype for function get_dna_complement that
 accepts a string and returns a string
 */
 
+std::string get_dna_complement(std::string dna);
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
-
+#include <iostream>
+#include "dna.h"
 //write using statements
-
+using std::cout;    using std::cin;
+using std::string;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +14,39 @@ user enters a y or Y.
 */
 int main() 
 {
-	return 0;
+	string dna;
+  string user_choice;
+  char quit_prog;
+
+  do
+  {
+    
+    cout<<"Enter 1 for GC Content. Enter 2 for DNA Complement: \n";
+    cin >> user_choice;
+
+    if (user_choice == "1")
+    {
+      cout<<"Enter a DNA strand (using all caps) \n";
+      cin >> dna;
+      cout<<"The GC Content is: " << get_gc_content(dna) << "\n";
+    }
+    else if(user_choice == "2")
+    {
+      cout<<"Enter a DNA strand (using all caps) \n";
+      cin >> dna;
+      cout<<"The DNA Complement is: " << get_dna_complement(dna) << "\n";
+    }
+    else
+    {
+      cout<<"You entered an invalid menu selection \n";
+    }
+
+    cout<<"Would you like to quit program? Type 'Y' or 'y' to quit. \n";
+    cout<<"Enter any other key to continue: \n";
+    cin >> quit_prog;
+
+  }
+  while(!(quit_prog == 'Y' || quit_prog == 'y'));
+  
+  return 0;
 }
